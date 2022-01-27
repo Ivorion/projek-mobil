4ed0824a190bb3bd40061205488a7d3d717b48aa
= <richardoehbet221@gmail.com>
2022-01-27 12:56:27 +0700

    tambah listbarang

--- my-app.js
+++ my-app.js
@@ -34,7 +34,7 @@ app = new Framework7({
     {
       path: "/menu/",
       url: "/pages/menu.html",
-    }
+    },
   ],
   // ... other parameters
 });
@@ -42,134 +42,27 @@ app = new Framework7({
 var mainView = app.views.create(".view-main");
 var $$ = Dom7;
 
-$$(".view-main").on("click", "#simpan", () => {
-  var id_barang = $$("#id_barang").val();
-  var id_pelanggan = $$("#id_pelanggan").val();
-  var qty = $$("#qty").val();
-  var total_sub = $$("#total_sub").val();
+$$(document).on(
+  "page:init",
+  '.page[data-name="listbarang"]',
+  function (e, page) {
+    // const el = page.$el;
+    app.request.json("http://localhost:8080/projek/barang/baca.php", (data) => {
+      const listBarang = data.map(
+        (barang) => `
+      <div class="card demo-card-header-pic">
+        <div style="background-image:url(microphone.jpg)" class="card-header align-items-flex-end"></div>
+          <div class="card-content card-content-padding">
+            <p class="date">124 Buah</p>
+            <p>${barang.nama_barang}</p>
+            <p>Rp. ${barang.harga}</p>
+          </div>
+        <div class="card-footer"><a href="#" class="link">Tambah ke keranjang</a></div>
+      </div>
+    `
+      );
 
-  app.request({
-    url: "http://localhost:8080/toko/tambah.php",
-    type: "POST",
-    data: {
-      id_barang: id_barang,
-      id_pelanggan: id_pelanggan,
-      qty: qty,
-      total_sub: total_sub,
-    },
-
-    dataType: "json",
-    success(data) {
-      if (data.pesan) {
-        app.dialog.alert(data.pesan);
-        $$("#id_barang").val("");
-        $$("#qty").val("");
-        $$("#total_sub").val("");
-        $$("#id_pelanggan").val();
-        tampil();
-        app.views.main.router.navigate("/home/");
-      }
-    },
-  });
-});
-
-$$(".view-main").on("click", "#ubah", function () {
-  var id = $$(this).data("id");
-  app.views.main.router.navigate("/ubah/");
-  app.request.json(
-    "http://localhost:8080/toko/cari.php",
-    { id: id },
-    function (data) {
-      $$("#eid_barang").val(data[0].id_barang);
-      $$("#eqty").val(data[0].qty);
-      $$("#eid_pelanggan").val(data[0].id_pelanggan);
-      $$("#etotal_sub").val(data[0].total_sub);
-      $$("#kode").val(data[0].id_keranjang);
-    }
-  );
-});
-
-$$(".view-main").on("click", "#update", () => {
-  var id_barang = $$("#eid_barang").val();
-  var id_pelanggan = $$("#eid_pelanggan").val();
-  var qty = $$("#eqty").val();
-  var total_sub = $$("#etotal_sub").val();
-  var id = $$("#kode").val();
-
-  app.request({
-    url: "http://localhost:8080/toko/ubah.php",
-    type: "POST",
-    data: {
-      id_barang: id_barang,
-      id_pelanggan: id_pelanggan,
-      qty: qty,
-      total_sub: total_sub,
-      id: id,
-    },
-    dataType: "json",
-    success(data) {
-      if (data.pesan) {
-        app.dialog.alert(data.pesan);
-        $$("#eid_barang").val("");
-        $$("#eid_pelanggan").val("");
-        $$("#eqty").val("");
-        $$("#etotal_sub").val("");
-        tampil();
-        app.views.main.router.navigate("/home/");
-      }
-    },
-  });
-});
-
-const tampil = () => {
-  app.request.json("http://localhost:8080/toko/index.php", (data) => {
-    const tabel = data.map(
-      (keranjang, index) => `
-        <tr>
-            <td>${index + 1}</td>
-            <td>
-                <a class='link color-red' data-id='${
-                  keranjang.id_keranjang
-                }' id='hapus'>
-                    <i class='f7-icons size-20'>trash</i>
-                </a>
-                <a class='link color-teal' data-id='${
-                  keranjang.id_keranjang
-                }' id='ubah'>
-                    <i class='f7-icons size-20'>pencil</i>
-                </a>
-            </td>
-            <td>${keranjang.id_barang}</td>
-            <td>${keranjang.qty}</td>
-            <td>${keranjang.total_sub}</td>
-            <td>${keranjang.id_pelanggan}</td>
-            <td>${keranjang.tgl_keranjang}</td>
-        </tr>
-      `
-    );
-
-    $$("#tampil").html(tabel.join(""));
-  });
-};
-
-tampil();
-
-function baca() {
-  app.request.json("http://localhost/projek/barang/baca.php", function (data) {
-   var jlh = data.length;
-   console.log(data);
-   var i = 0;
-   var buatTabel = "";
-   for (i = 0; i < jlh; i++){
-       buatTabel += "<tr>" +
-           "<td>" + (i + 1) + "</td>" +
-           "<td>" + data[i].isbn + "</td>" +
-           "<td>" + data[i].judul_buku + "</td>" +
-           "<td>" + data[i].penerbit + "</td>" +
-           "<td>" + data[i].penulis + "</td>" +
-           "<td>" + data[i].tahun + "</td>" +
-           "</tr>";
-   }
-   $$("#tampil").html(buatTabel);
-});
-}
+      $$(".listbarang-container").html(listBarang);
+    });
+  }
+);
